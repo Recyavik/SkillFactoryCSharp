cd78bffd60e71ea31cc9c3f45bf20b1d625d261f
Recyavik <vkataev71@gmail.com>
2023-05-08 19:38:16 +0800

    SkillFactoryCSharp-Module3_2: Максимальное т минимальное значение по типу

--- Module 3_3/Program.cs
+++ Module 3_3/Program.cs
@@ -1,9 +1,3 @@
-﻿string MyName = "Jane";
-byte MyAge = 27;
-bool HaveIApet = true;
-double MyShoeSize = 37.5;
-
-Console.WriteLine("My name is " + MyName);
-Console.WriteLine("MyAge " + MyAge);
-Console.WriteLine("Do I have a pet? " + HaveIApet);
-Console.WriteLine("My shoe size is " + MyShoeSize);
+﻿
+Console.WriteLine("IntMin {0} ", int.MinValue);
+Console.WriteLine("IntMax {0} ", int.MaxValue);
