c424160e66ff12cf5f7e9ccf95bdefbac2f7181e
Recyavik <vkataev71@gmail.com>
2023-05-08 19:34:15 +0800

    SkillFactoryCSharp-Module3: Другие типы данных

---
+++ Module 3_3/Program.cs
@@ -0,0 +1,9 @@
+﻿string MyName = "Jane";
+byte MyAge = 27;
+bool HaveIApet = true;
+double MyShoeSize = 37.5;
+
+Console.WriteLine("My name is " + MyName);
+Console.WriteLine("MyAge " + MyAge);
+Console.WriteLine("Do I have a pet? " + HaveIApet);
+Console.WriteLine("My shoe size is " + MyShoeSize);
