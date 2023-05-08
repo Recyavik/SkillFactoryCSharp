a20f87f9324aa1199db9391ab0a340e95e9a3380
Recyavik <vkataev71@gmail.com>
2023-05-08 19:12:13 +0800

    Module 3_2_7

---
+++ Module_3_2_7/Program.cs
@@ -0,0 +1,15 @@
+﻿const string MyName = "Viktor";
+
+Console.WriteLine("Привет,\n мир!");
+Console.WriteLine(MyName);
+
+Console.WriteLine("\t Привет Мир");
+Console.WriteLine("\t Мне 27 лет");
+Console.WriteLine("\t My name is \n {0}", MyName);
+Console.WriteLine("\u0040");
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine(5);
+Console.WriteLine(0x0A); // Шестнадцатиричный код
+Console.WriteLine(0b11); // Двоичный
+Console.WriteLine(5.5); // десятичный
