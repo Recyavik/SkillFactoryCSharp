7f84a00c54accb6239a7785e65c1f9e40bc0f42a
Recyavik <vkataev71@gmail.com>
2023-05-10 17:37:34 +0800

    SF-Module_4_2: Цикл do и бесконечный цикл

--- Module_4_2/Program.cs
+++ Module_4_2/Program.cs
@@ -45,6 +45,52 @@
                 Console.WriteLine(z);
                 z--;
             } while (z != 0);
+            Console.WriteLine("Цикл do");
+            int t = 0;
+
+            do
+            {
+                Console.WriteLine(t);
+
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
+                t++;
+            } while (t < 3);
+
+
+            // бесконечный цикл
+            do
+            {
+                Console.WriteLine("Бесконечный цикл");
+            } while (true);
+
 
         }
     }
