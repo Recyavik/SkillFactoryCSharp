bb7eee65a9bb4943e9d937d8923ef6930bc30242
Recyavik <vkataev71@gmail.com>
2023-05-10 11:49:07 +0800

    SF-Module_3_7: Преобразование типов и неявная типизация

--- Module_3_7/Program.cs
+++ Module_3_7/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Module_3_7
+﻿using System.Xml.Linq;
+
+namespace Module_3_7
 {
     internal class Program
     {
@@ -7,7 +9,38 @@
             Console.Write("Enter your age: ");
             int age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your age is {0} ", age);
+          
+
+
+            Console.Write("Enter your age: ");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your age is {0} ", age);
+
+            Console.Write("Enter your age: ");
+            bool iscorrect = int.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("Your age is {0} ", age);
+
+            Console.Write("Enter your favotit day: ");
+            DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine(day);
+
+            // Неявная типизация
+            var name = "Viktor";
+            var answer = 2 + 2;
+
+            age = checked((byte)int.Parse(Console.ReadLine()));
+            int intage = age;
+            Console.WriteLine("Your name is {0} and age is {1} ", name, intage);
+            var howItall = 162;
+            var myshoe = 37.5;
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.WriteLine("What is my grouth? {0} sm", howItall);
+            Console.WriteLine("What is my shoe size? {0}", myshoe);
+
             Console.ReadKey();
+
+
+
         }
     }
 }
