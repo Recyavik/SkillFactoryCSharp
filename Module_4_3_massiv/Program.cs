ce54b75c622f8896ab9acce2499e5bfc68ff8cbd
Recyavik <vkataev71@gmail.com>
2023-05-10 18:36:42 +0800

    SF-Module_4_3: Цикл перечислений foreach

---
+++ Module_4_3_massiv/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Module_4_3_massiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] favcolors = new string[3];
+            for (int i = 0; i<favcolors.Length; i++)
+            {
+                Console.WriteLine("Введите {0}-й любимый цвет",i+1);
+                favcolors[i] = Console.ReadLine();
+            }
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
+        }
+       
+    }
+}
