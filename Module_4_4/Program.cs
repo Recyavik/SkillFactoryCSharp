2364efbf1c1abc13d0373ac8a67ce7929b7e1969
Recyavik <vkataev71@gmail.com>
2023-05-10 21:32:20 +0800

    SF-Module_4_4:  Ввод кортежа

--- Module_4_4/Program.cs
+++ Module_4_4/Program.cs
@@ -19,6 +19,30 @@
             Console.WriteLine("Ваше имя: {0}", anketa2.name);
             Console.WriteLine("Ваш возраст: {0}", anketa2.age);
 
+            var (name, age) = ("Евгения", 27);
+
+            Console.WriteLine("Моё имя: {0}", name);
+            Console.WriteLine("Мой возраст: {0}", age);
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите возраст с цифрами:");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
+
+            (string Name, string Type, double Age, int NameCount) Pet;
+            Console.WriteLine("Введите имя питомца");
+            Pet.Name = Console.ReadLine();
+            Pet.NameCount = Pet.Name.Length;
+
+            Console.WriteLine("Введите вид питомца");
+            Pet.Type = Console.ReadLine();
+
+            Console.WriteLine("Введите возрас питомца");
+            Pet.Age = double.Parse(Console.ReadLine());
+
             Console.ReadKey();
         }
     }
