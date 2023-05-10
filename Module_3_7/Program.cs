67c669296f2ed76733c35e1c221b7b97b7f5b7a9
Recyavik <vkataev71@gmail.com>
2023-05-10 11:27:27 +0800

    SF-Module3_7 Преобразование типов

---
+++ Module_3_7/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Module_3_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is {0} ", age);
+            Console.ReadKey();
+        }
+    }
+}
