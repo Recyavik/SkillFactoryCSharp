774d7c45283e9a45c33cb7dcc168e488b7f9654b
Recyavik <vkataev71@gmail.com>
2023-05-10 17:14:38 +0800

    SF-Module_4_2: Цикл со счётчиком

---
+++ Module_4_2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Module_4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i<=5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (double i = 5.0; i >= 0; i -= 1)
+            {
+                Console.WriteLine(i);
+            }
+
+            int j = 1;
+            for (; j <=3 ; )
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+
+        }
+    }
+}
