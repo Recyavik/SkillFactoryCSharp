daf11cbecfde3324a3484b9e6f88aa53e726bf20
Recyavik <vkataev71@gmail.com>
2023-05-10 18:38:57 +0800

    SF-Module_4_3: Инициализация массива и Цикл перечислений foreach

--- Module_4_3_massiv/Program.cs
+++ Module_4_3_massiv/Program.cs
@@ -4,15 +4,19 @@
     {
         static void Main(string[] args)
         {
-            string[] favcolors = new string[3];
-            for (int i = 0; i<favcolors.Length; i++)
+            //Размерность массива 
+            var arr = new int[] { 1, 2, 3, 4 };
+            var l = arr.Length;
+
+            //Разные виды инициализации массива
+            int[] arr1 = new int[4] { 1, 2, 3, 5 };
+            int[] arr2 = new int[] { 1, 2, 3, 5 };
+            int[] arr3 = new[] { 1, 2, 3, 5 };
+            int[] arr4 = { 1, 2, 3, 5 };
+
+            foreach (int item in arr1) 
             {
-                Console.WriteLine("Введите {0}-й любимый цвет",i+1);
-                favcolors[i] = Console.ReadLine();
-            }
-            foreach (var color in favcolors)
-            {
-                Console.WriteLine(color);
+                Console.WriteLine(item);
             }
         }
        
