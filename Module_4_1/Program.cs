8918f4cf0f3b50640bf1984ba3913cd57a922bdb
Recyavik <vkataev71@gmail.com>
2023-05-10 12:57:52 +0800

    SF-Module_4: Логические операции

---
+++ Module_4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Module_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myapples = 5;
+            var hisapples = 6;
+            var hispeares = 5;
+
+            var result = (myapples != hispeares) & (myapples < hisapples);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
