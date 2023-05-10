a6faaf77bf0395cfccb315d2092af2e54cd08726
Recyavik <vkataev71@gmail.com>
2023-05-10 21:37:21 +0800

    SF-Module_4_4:  Ввод кортежа пользователя

--- Module_4_4/Program.cs
+++ Module_4_4/Program.cs
@@ -43,6 +43,52 @@
             Console.WriteLine("Введите возрас питомца");
             Pet.Age = double.Parse(Console.ReadLine());
 
+            // Кортеж пользователя
+            (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+
+            for (int k = 0; k < 3; k++)
+
+            {
+
+                Console.WriteLine("Введите имя");
+                User.Name = Console.ReadLine();
+
+                Console.WriteLine("Введите фамилию");
+
+                User.LastName = Console.ReadLine();
+
+                Console.WriteLine("Введите логин");
+
+                User.Login = Console.ReadLine();
+
+                User.LoginLength = User.Login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+
+                var result = Console.ReadLine();
+
+                if (result == "Да")
+                {
+                    User.HasPet = true;
+                }
+                else
+                {
+                    User.HasPet = false;
+                }
+
+                Console.WriteLine("Введите возраст пользователя");
+
+                User.Age = double.Parse(Console.ReadLine());
+
+                User.favcolors = new string[3];
+                Console.WriteLine("Введите три любимых цвета пользователя");
+
+                for (int i = 0; i < User.favcolors.Length; i++)
+                {
+                    User.favcolors[i] = Console.ReadLine();
+                }
+            }
+
             Console.ReadKey();
         }
     }
