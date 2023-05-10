133502c6af66ebb3f84d4f7f1c1aadd05c469e83
Recyavik <vkataev71@gmail.com>
2023-05-10 11:13:39 +0800

    SF-Module 3_6 Арифметические операции

---
+++ Module 3_6/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Module_3_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 10;
+            Console.WriteLine("Value: {0} Increment: {1}", counter, ++counter);
+            double result = 5.0 / 2.0;
+            Console.WriteLine("5 / 2 = {0}", result);
+            int res = byte.MaxValue + int.MinValue;
+            Console.WriteLine(res);
+            
+
+        }
+    }
+}
