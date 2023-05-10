91388aa1e1881f3daab81ad9738b9916b0d22728
Recyavik <vkataev71@gmail.com>
2023-05-10 14:20:21 +0800

    SF-Module4_1: Операторы выбора switch

--- Module_4_1/Program.cs
+++ Module_4_1/Program.cs
@@ -113,6 +113,35 @@
 
                 Console.WriteLine("Your color is cyan!");
             }
+
+            // Оператор выбора
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+            }
         }
     }
 }
