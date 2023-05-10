86eabdd0155afe5618cfe46bab591dfc6013aa40
Recyavik <vkataev71@gmail.com>
2023-05-10 18:49:41 +0800

    SF-Module_4_3:  Посимвольный вывод (строка = массив символов) Цикл перечислений foreach

--- Module_4_3_massiv/Program.cs
+++ Module_4_3_massiv/Program.cs
@@ -18,6 +18,37 @@
             {
                 Console.WriteLine(item);
             }
+
+            var numbers = new int[] { 6, 30, 7, 14, 2, 15 };
+            int minValue = 100000,
+            minIndex = -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < minValue)
+                {
+                    minValue = numbers[i];
+                    minIndex = i;
+                }
+            }
+
+            Console.WriteLine("Наименьший элемент массива имеет индекс {0}, значение этого элемента = {1}", minIndex, numbers[minIndex]);
+            Console.WriteLine("Введите своё имя");
+
+            var name = "";
+            name = Console.ReadLine();
+
+            Console.WriteLine("Ваше имя по буквам: ");
+
+            foreach (var ch in name)
+            {
+                Console.Write(ch + " ");
+            }
+
+            Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
+
+            Console.ReadKey();
+
         }
        
     }
