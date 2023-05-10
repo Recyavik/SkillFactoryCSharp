c722c03359f6a4bf03c4c82eb9e4f1ad63c3a466
Recyavik <vkataev71@gmail.com>
2023-05-10 18:24:19 +0800

    SF-Module_4_2: Цикл do и бесконечный цикл с остановкой

--- Module_4_2/Program.cs
+++ Module_4_2/Program.cs
@@ -85,13 +85,26 @@
             } while (t < 3);
 
 
-            // бесконечный цикл
-            do
+            int sum = 0;
+            while (true)
             {
-                Console.WriteLine("Бесконечный цикл");
-            } while (true);
+                Console.WriteLine("Введите число");
+                var number = Convert.ToInt32(Console.ReadLine());
 
+                if (number < 0)
+                {
+                    continue;
+                }
+                else if (number == 0)
+                {
+                    break;
+                }
 
+                // number > 0
+                sum += number;
+            }
+
+            Console.WriteLine("Итоговая сумма: {0}", sum);
         }
     }
 }
