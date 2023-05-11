44007819858d2c65dcd3c6f9f9cc928d771965e0
Recyavik <vkataev71@gmail.com>
2023-05-11 14:50:18 +0800

    SF-Module_5_1:  Метод ввода массива и возврата отсортированного массива

--- Module_5_1/Program.cs
+++ Module_5_1/Program.cs
@@ -2,6 +2,31 @@
 {
     internal class Program
     {
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите {0}-й элемент массива ", i+1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+            int temp = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i+1; j < result.Length; j++)
+                {
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+                }
+            }
+            return result;
+        }
+
         static string ShowColor()
         {
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
@@ -53,6 +78,10 @@
             Console.WriteLine("Ваш возраст: {0}", age);
 
             Console.WriteLine(ShowColor());
+
+            GetArrayFromConsole();
+
+
 
         }
     }
