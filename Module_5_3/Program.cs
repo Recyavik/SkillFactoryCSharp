23859bd4539eacd9785f9a530901e58fd4544de5
Recyavik <vkataev71@gmail.com>
2023-05-18 20:38:00 +0800

    SF-Module_5_3:  int не передает новое значение

--- Module_5_3/Program.cs
+++ Module_5_3/Program.cs
@@ -5,14 +5,17 @@
         static void Main(string[] args)
         {
             var arr = new int[] { 1, 2, 3 };
-            BigDataOperation(arr);
+            var data = 5;
+            BigDataOperation(arr, data);
 
             Console.WriteLine(arr[0]);
+            Console.WriteLine(data);
         }
 
-        private static void BigDataOperation(int[] arr)
+        private static void BigDataOperation(int[] arr, int data)
         {
             arr[0] = 4;
+            data = 0;
         }
     }
 }
