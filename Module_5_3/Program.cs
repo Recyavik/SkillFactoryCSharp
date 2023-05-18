fbe0e69622e26edfed466f9a89b360b184d62146
Recyavik <vkataev71@gmail.com>
2023-05-18 20:32:09 +0800

    SF-Module_5_3:  int[] - передача поссылке

---
+++ Module_5_3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Module_5_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 2, 3 };
+            BigDataOperation(arr);
+
+            Console.WriteLine(arr[0]);
+        }
+
+        private static void BigDataOperation(int[] arr)
+        {
+            arr[0] = 4;
+        }
+    }
+}
