c898cfbbc28d7120e20c4c3b28982e6cbf77e601
Recyavik <vkataev71@gmail.com>
2023-05-20 09:27:48 +0800

    SF-Module_5_4:  Область видимости

--- Module_5_4/Program.cs
+++ Module_5_4/Program.cs
@@ -1,27 +1,23 @@
 ﻿namespace Module_5_4
 {
-
-    internal class Program
+    class Program
     {
-
-        static int num1 = 655;
-        static int Sum()
-        {
-            var num2 = 1; 
-            return num2 + num1;
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine(num1);
-            Console.WriteLine(Sum());
-            if (num1 > 0)
-            {
-                var num3 = 5;
-                Console.WriteLine(num1 + num3);
-            } 
+            var num = 1;
+            Matreshka(num);
+            Console.ReadKey();
         }
+        static int Matreshka(int num)
+        {
+            num++;
 
-      
+            return Sum(num, 3);
+        }
+        static int Sum(int num, int num2)
+        {
+            return num + num2;
+        }
+ 
     }
-
-}
+ }
