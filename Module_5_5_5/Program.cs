1c6b9fc18c0168427c468d4bdf59134103452af9
Recyavik <vkataev71@gmail.com>
2023-05-21 12:05:40 +0800

    SF-Module_5_5_5:  Рекурсивные функции Факториал

---
+++ Module_5_5_5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Module_5_5_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(20));
+        }
+        static decimal Factorial(int x) 
+        { 
+            if (x == 0) return 1;
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+    }
+}
