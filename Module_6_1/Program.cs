8f6482b5709cfa8e3fcb3261fe37b608ecc32ece
Recyavik <vkataev71@gmail.com>
2023-05-28 14:11:15 +0800

    SF-Module_6_1:  Объявление классов и структур

---
+++ Module_6_1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Module_6_1
+{
+    internal class Program
+    {
+        class Human
+        {
+            // Поля класса
+            public string name;
+            public int age;
+
+            //Метод класса
+            public void Greetings()
+            {
+                Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+            }
+        }
+        struct Animal
+        {
+            // Поля структуры
+            public string type;
+            public int age;
+            public string name;
+
+            // Метод структуры
+            public void Info()
+            {
+                Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
+            }
+
+        }
+            
+
+    }
+}
