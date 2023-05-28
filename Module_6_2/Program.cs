68ad292b1417fc974f1af344ab246d02c8777494
Recyavik <vkataev71@gmail.com>
2023-05-28 14:18:58 +0800

    SF-Module_6_2:  Объявление классов и структур 0 и null

---
+++ Module_6_2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Module_6_2
+{
+    class Human
+        {
+            // Поля класса
+            public string name;
+            public int age;
+
+            // Метод класса
+            public void Greetings()
+            {
+                Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+            }
+        }
+    struct Animal
+    {
+        // Поля структуры
+        public string type;
+        public string name;
+        public int age;
+
+        // Метод структуры
+        public void Info()
+        {
+            Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
+        }
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+            human.Greetings();
+
+            human.name = "Дмитрий";
+            human.age = 23;
+            human.Greetings();
+
+
+            Console.ReadKey();
+        }
+    }
+}
