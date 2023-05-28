c716565018b87a5350a41948272467a85910d0c9
Recyavik <vkataev71@gmail.com>
2023-05-28 15:17:38 +0800

    SF-Module_6_2_8:  Объявление классов. Конструктор класса и структуры

---
+++ Module_6_2_8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+
+namespace Module_6_2_8
+{
+    class Rectangle
+    {
+        public int a;
+        public int b;
+
+        public decimal Square()
+        {
+            return a * b;
+        }
+
+        public Rectangle(int da, int db)
+        {
+            a = da;
+            b = db;
+        }
+        public Rectangle(int da)
+        {
+            a = da;
+            b = da;
+        }
+        public Rectangle()
+        {
+            a = 6;
+            b = 4;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            Rectangle r1 = new Rectangle(5);
+            Rectangle r2 = new Rectangle(7,12);
+            Console.WriteLine(r.Square());
+            Console.WriteLine(r1.Square());
+            Console.WriteLine(r2.Square());
+        }
+    }
+}
