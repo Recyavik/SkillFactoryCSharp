b6d8020c0b63ca10782bb6fded4148eac93c2cfd
Recyavik <vkataev71@gmail.com>
2023-05-28 20:35:29 +0800

    SF-Module_6_3_2:  Всегда проверяй классы на наличие значений null

---
+++ Module_6_3_2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Module_6_3_2
+{
+    class Bus
+    {
+        public int? Load;
+
+        public void PrintStatus()
+        {
+            if (Load.HasValue)
+            {
+                Console.WriteLine("В авбтобусе {0} пассажиров", Load.Value);
+            }
+            else
+            {
+                Console.WriteLine("Автобус пуст!");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bus bus = new Bus();
+            bus.PrintStatus();
+            bus.Load = 2;
+            bus.PrintStatus();
+            bus.Load = null;
+            bus.PrintStatus();
+        }
+    }
+}
