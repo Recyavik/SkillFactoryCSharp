b24de50bf074e2f09e9495bb005b6d7ece81effe
Recyavik <vkataev71@gmail.com>
2023-05-31 18:45:15 +0800

    SF-Module_7_1_11:  Восходящее преобразование классов

---
+++ Module_7_1_11/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Module_7_1_11
+{
+    class Creature { }
+
+    class Animal : Creature { }
+
+    class Human : Creature { }
+
+    class HomoSapiens : Human { }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HomoSapiens hs = new HomoSapiens();
+            Human human = hs;
+            Creature creature = (Creature)human;
+            Creature secondCreature = new Animal();
+        }
+    }
+}
