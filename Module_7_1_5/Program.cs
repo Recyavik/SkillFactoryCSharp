5064a342397e7a9501f86c84f83583ddb9e5151b
Recyavik <vkataev71@gmail.com>
2023-05-31 13:18:56 +0800

    SF-Module_7_1_5:  Пример наследования фрукты

---
+++ Module_7_1_5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module_7_1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
