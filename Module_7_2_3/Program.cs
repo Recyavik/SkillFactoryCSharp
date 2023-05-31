6459c4f1ba441d5512d5a2c6045822ccbe7cacb9
Recyavik <vkataev71@gmail.com>
2023-05-31 20:42:39 +0800

    SF-Module_7_2_3:  Полиморфизм virtual и override

---
+++ Module_7_2_3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Module_7_2_3
+{
+    class BaseClass
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Метод класса BaseClass");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Метод класса DerivedClass");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass bc = new BaseClass();
+            DerivedClass dk = new DerivedClass();
+            bc.Display();
+            dk.Display();
+            ((BaseClass)dk).Display();
+
+
+        }
+    }
+}
