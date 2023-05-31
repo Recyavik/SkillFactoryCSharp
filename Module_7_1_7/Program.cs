f2522bdddf314408904cd47921cd384a0a53318a
Recyavik <vkataev71@gmail.com>
2023-05-31 14:29:24 +0800

    SF-Module_7_1_8:  Конструкторы не передаются произвольному классу при наследовании. Если в базовом классе не определен конструктор по умолчанию без параметров, а только конструкторы с параметрами, то в производном классе мы обязательно должны вызвать один из этих конструкторов через ключевое слово base.

---
+++ Module_7_1_7/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Module_7_1_7
+{
+    class SmartHelper 
+    {
+        private string name;
+
+    public SmartHelper(string name)
+    {
+        this.name = name;
+    }
+
+    public void Greetings(string name)
+    {
+        Console.WriteLine("Привет, {0}, я интеллектуальный помощник {1}", name, this.name);
+    }
+}
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartHelper helper = new SmartHelper("Олег");
+            helper.Greetings("Грег");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module_7_1_8/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Module_7_1_8
+{
+    class BaseClass
+    {
+        private int value;
+
+        public BaseClass(int value)
+        {
+            this.value = value;
+        }
+    }
+    class InheritedClass : BaseClass
+    {
+        private int newValue;
+
+        public InheritedClass(int newValue) : base(100)
+        {
+            this.newValue = newValue;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
