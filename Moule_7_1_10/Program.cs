374ceb585a5853280d894cded6d91e01c93df73d
Recyavik <vkataev71@gmail.com>
2023-05-31 14:45:34 +0800

    SF-Module_7_1_10:  Пример конструктора класса c доступом this and base

---
+++ Moule_7_1_10/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Moule_7_1_10
+{
+    class BaseClass
+    {
+        protected string Name;
+
+        public BaseClass(string name)
+        {
+            Name = name;
+        }
+    }
+    class DerivedClass : BaseClass
+    {
+        public string Description;
+
+        public int Counter;
+
+        public DerivedClass(string name, string description) : base(name)
+        {
+            Description = description;
+        }
+        public DerivedClass(string name, string description, int counter) : this(name, description)
+        {
+            Counter = counter;
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
