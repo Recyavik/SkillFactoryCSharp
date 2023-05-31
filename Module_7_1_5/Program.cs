a6e5c206818473da5b24db36fc504eb52ac97887
Recyavik <vkataev71@gmail.com>
2023-05-31 13:48:37 +0800

    SF-Module_7_1_6:  Пример конструктора класса при совпадение имен

--- Module_7_1_5/Program.cs
+++ Module_7_1_5/Program.cs
@@ -1,5 +1,37 @@
 ﻿namespace Module_7_1_5
 {
+    class Food
+    {
+
+    }
+    class Fruits: Food
+    {
+
+    }
+    class Apple: Fruits
+    {
+
+    }
+    class Banana: Fruits
+    {
+
+    }
+    class Pear: Fruits
+    {
+
+    }
+    class Vegetables: Food
+    {
+
+    }
+    class Patato: Vegetables
+    {
+
+    }
+    class Carrot: Vegetables
+    {
+
+    }
     internal class Program
     {
         static void Main(string[] args)
---
+++ Module_7_1_6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Module_7_1_6
+{
+    class Obj
+    {
+        private string name;
+        private string owner;
+        private int length;
+        private int count;
+
+        public Obj(string name, string ownerName, int objLength, int count)
+        {
+            this.name = name;
+            owner = ownerName;
+            length = objLength;
+            this.count = count;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
