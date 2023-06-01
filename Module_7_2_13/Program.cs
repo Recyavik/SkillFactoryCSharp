3962531ab04ad287cb581732c04c9565d8a6c469
Recyavik <vkataev71@gmail.com>
2023-06-01 19:17:36 +0800

    SF-Module_7_2_14: Индексатор

--- Module_7_2_13/Program.cs
+++ Module_7_2_13/Program.cs
@@ -1,88 +1,87 @@
 ﻿namespace Module_7_2_13
 {
-class Book
-{
-	public string Name;
-	public string Author;
-}
+    class Book
+    {
+        public string Name;
+        public string Author;
+    }
 
-class BookCollection
-{
-	private Book[] collection;
+    class BookCollection
+    {
+        private Book[] collection;
 
-	public BookCollection(Book[] collection)
-	{
-		this.collection = collection;
-	}
+        public BookCollection(Book[] collection)
+        {
+            this.collection = collection;
+        }
 
-	// Индексатор по массиву
-	public Book this[int index]
-	{
-		get
-		{
-			// Проверяем, чтобы индекс был в диапазоне для массива
-			if (index >= 0 && index < collection.Length)
-			{
-				return collection[index];
-			}
-			else
-			{
-				return null;
-			}
-		}
+        // Индексатор по массиву
+        public Book this[int index]
+        {
+            get
+            {
+                // Проверяем, чтобы индекс был в диапазоне для массива
+                if (index >= 0 && index < collection.Length)
+                {
+                    return collection[index];
+                }
+                else
+                {
+                    return null;
+                }
+            }
 
-		private set
-		{
-			// Проверяем, чтобы индекс был в диапазоне для массива
-			if (index >= 0 && index < collection.Length)
-			{
-				collection[index] = value;
-			}
-		}
-	}
+            private set
+            {
+                // Проверяем, чтобы индекс был в диапазоне для массива
+                if (index >= 0 && index < collection.Length)
+                {
+                    collection[index] = value;
+                }
+            }
+        }
 
-	public Book this[string name]
-	{
-		get
-		{
-			for (int i = 0; i < collection.Length; i++)
-			{
-				if (collection[i].Name == name)
-				{
-					return collection[i];
-				}
-			}
-			return null;
-		}
-	}
-}
-
+        public Book this[string name]
+        {
+            get
+            {
+                for (int i = 0; i < collection.Length; i++)
+                {
+                    if (collection[i].Name == name)
+                    {
+                        return collection[i];
+                    }
+                }
+                return null;
+            }
+        }
+    }
 class Program
-{
-	static void Main(string[] args)
-	{
+    {
+        static void Main(string[] args)
+        {
 
-		var array = new Book[] {
-			new Book { Name = "Мастер и Маргарита", Author = "М.А. Булгаков" },
-			new Book { Name = "Отцы и дети", Author = "И.С. Тургенев" },
-		};
-		BookCollection collection = new BookCollection(array);
-			Console.WriteLine(collection);
-			Console.ReadKey();
+            var array = new Book[] {
+            new Book { Name = "Мастер и Маргарита", Author = "М.А. Булгаков" },
+            new Book { Name = "Отцы и дети", Author = "И.С. Тургенев" },
+        };
+            BookCollection collection = new BookCollection(array);
 
-		Book book = collection[1];
-			Console.WriteLine(book.Name);
+            Console.ReadKey();
 
-		book = collection[-1];
-			Console.WriteLine(book.Name);
+            Book book = collection[1];
+            Console.WriteLine(book.Name);
 
-        book = collection[4];
+            book = collection[-1];
+            Console.WriteLine(book.Name);
+
+            book = collection[4];
             Console.WriteLine(book.Name);
             Console.ReadKey();
 
-		book = collection["Мастер и Маргарита"];
+            book = collection["Мастер и Маргарита"];
             Console.WriteLine(book.Name);
             Console.ReadKey();
-	}
-}
+        }
+    }
 }
---
+++ Module_7_2_14/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Module_7_2_14
+{
+    class IndexingClass
+    {
+        private int[] array;
+
+        public IndexingClass(int[] array)
+        {
+            this.array = array;
+        }
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                array[index] = value;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
