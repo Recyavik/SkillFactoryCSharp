64c1be5ca04f749beb587fbccede3ef10f045bc7
Recyavik <vkataev71@gmail.com>
2023-06-01 18:11:50 +0800

    SF-Module_7_2_12:  Перегрузка операторов

---
+++ Module_7_2_12/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Module_7_2_12
+{
+/*    Для класса Obj перегрузите операторы + и -, чтобы результатом работы оператора 
+ *    был новый экземпляр класса Obj, а операции производились над полем Value
+*/
+    class Obj
+    {
+        public int Value;
+
+        public static Obj operator +(Obj a, Obj b)
+        {
+            return new Obj
+            {
+                Value = a.Value + b.Value
+            };
+        }
+        public static Obj operator -(Obj a, Obj b)
+        {
+            return new Obj
+            {
+                Value = a.Value - b.Value
+            };
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Obj obj = new Obj { Value = 12 };
+            
+            Console.WriteLine(obj.Value + obj.Value);
+        }
+    }
+}
