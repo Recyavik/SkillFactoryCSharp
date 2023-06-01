e0a39e0dbe0c342f5cab0136227ad411b2c37e31
Recyavik <vkataev71@gmail.com>
2023-06-01 13:44:51 +0800

    SF-Module_7_2_8:  Перегрузка методов

---
+++ Module_7_2_8/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Module_7_2_8
+{
+    internal class Program
+    {
+        static long Sum(long a, long b)
+        {
+            return a + b;
+        }
+
+        static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        static void Main(string[] args)
+        {
+            double a = 5.2, b = 10.0;
+            double answer = Sum(a,b);
+            Console.WriteLine(answer);
+        }
+    }
+}
