8ed31f1ec856882f62583a510387c0df6d2f82ce
Recyavik <vkataev71@gmail.com>
2023-06-01 19:03:47 +0800

    SF-Module_7_2_13: Индексаторы по индексу и имени

--- Module_7_2_13/Program.cs
+++ Module_7_2_13/Program.cs
@@ -1,10 +1,88 @@
 ﻿namespace Module_7_2_13
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+class Book
+{
+	public string Name;
+	public string Author;
+}
+
+class BookCollection
+{
+	private Book[] collection;
+
+	public BookCollection(Book[] collection)
+	{
+		this.collection = collection;
+	}
+
+	// Индексатор по массиву
+	public Book this[int index]
+	{
+		get
+		{
+			// Проверяем, чтобы индекс был в диапазоне для массива
+			if (index >= 0 && index < collection.Length)
+			{
+				return collection[index];
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		private set
+		{
+			// Проверяем, чтобы индекс был в диапазоне для массива
+			if (index >= 0 && index < collection.Length)
+			{
+				collection[index] = value;
+			}
+		}
+	}
+
+	public Book this[string name]
+	{
+		get
+		{
+			for (int i = 0; i < collection.Length; i++)
+			{
+				if (collection[i].Name == name)
+				{
+					return collection[i];
+				}
+			}
+			return null;
+		}
+	}
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+
+		var array = new Book[] {
+			new Book { Name = "Мастер и Маргарита", Author = "М.А. Булгаков" },
+			new Book { Name = "Отцы и дети", Author = "И.С. Тургенев" },
+		};
+		BookCollection collection = new BookCollection(array);
+			Console.WriteLine(collection);
+			Console.ReadKey();
+
+		Book book = collection[1];
+			Console.WriteLine(book.Name);
+
+		book = collection[-1];
+			Console.WriteLine(book.Name);
+
+        book = collection[4];
+            Console.WriteLine(book.Name);
+            Console.ReadKey();
+
+		book = collection["Мастер и Маргарита"];
+            Console.WriteLine(book.Name);
+            Console.ReadKey();
+	}
+}
 }
