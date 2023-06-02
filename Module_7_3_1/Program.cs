687923d6c940670105f3291772338e693553806c
Recyavik <vkataev71@gmail.com>
2023-06-02 11:31:27 +0800

    SF-Module_7_3_1: Абстрактные классы и их параметры

---
+++ Module_7_3_1/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Module_7_3_1
+{
+    abstract class Person
+    {
+        public string Name;
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void DisplayName()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+    class Employee : Person
+    {
+        // Булевый флаг, сообщающий, находится ли сотрудник на смене
+        public bool IsOnShift;
+
+        public Employee(string name, bool isOnShift) : base(name)
+        {
+            IsOnShift = isOnShift;
+        }
+    }
+    class Guest : Person
+    {
+        // Дата и время прибытия гостя
+        public DateTime ArrivalDate;
+
+        public Guest(string name, DateTime arrivalDate) : base(name)
+        {
+            ArrivalDate = arrivalDate;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Николай", true);
+            Guest guest = new Guest("Андрей", new DateTime(2020, 11, 05));
+
+            Person person = employee;
+            person.DisplayName();
+            employee.DisplayName();
+
+            person = guest;
+            person.DisplayName();
+            employee.DisplayName();
+        }
+    }
+}
