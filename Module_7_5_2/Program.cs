15bc33a6f5123dd91cfa32a702fdeebf10be9944
Recyavik <vkataev71@gmail.com>
2023-06-03 13:33:36 +0800

    SF-Module_7_5_2: Sum - статический метод класса

---
+++ Module_7_5_2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Module_7_5_2
+{
+    class Helper
+    {
+        public static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = Helper.Sum(1, 2, 3);
+            Console.WriteLine(sum);
+        }
+    }
+}
