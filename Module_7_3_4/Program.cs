1d1a33073e0f3ceb01a6427c4edbdcf108abc57e
Recyavik <vkataev71@gmail.com>
2023-06-03 11:17:55 +0800

    SF-Module_7_4: Агрегация и композиция Материнская плата может существовать без системного блока - агрегация(слабая связь), а Мозг  человека без человека не может - композиция (сильная связь)

--- Module_7_3_4/Program.cs
+++ Module_7_3_4/Program.cs
@@ -1,10 +1,61 @@
 ﻿namespace Module_7_3_4
 {
+    abstract class Transport
+    {
+        public abstract void Move();
+    }
+
+    class Boat : Transport
+    {
+        public override void Move()
+        {
+            // ...
+        }
+    }
+
+    class Car : Transport
+    {
+        public double Fuel;
+
+        public int Mileage;
+
+        public Car()
+        {
+            Fuel = 50;
+            Mileage = 0;
+        }
+        public override void Move()
+        {
+            Mileage++;
+            Fuel -= 0.5;
+        }
+
+        public void FillTheCar()
+        {
+            Fuel = 50;
+        }
+    }
+
+    enum FuelType
+    {
+        Gas = 0,
+        Electricity
+    }
+
+    class HybridCar : Car
+    {
+        public FuelType FuelType;
+
+        public void ChangeFuelType(FuelType type)
+        {
+            FuelType = type;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          
         }
     }
 }
---
+++ Module_7_4_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using Module_7_4_3;
+
+namespace Module_7_4_3
+{
+
+    // Агрегация
+    
+   public class A 
+    {
+    }
+public class B
+{
+    public A a;
+
+    public B(A a)
+    {
+        this.a = a;
+    }
+}
+    // Композиция
+  public class KA {}
+
+public class KB
+{
+    public KA a;
+
+    public KB()
+    {
+        a = new KA();
+    }
+}
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
