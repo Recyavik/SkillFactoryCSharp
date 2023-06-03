d03b0a050b136bd77a199bd4b88bd529d63f64dc
Recyavik <vkataev71@gmail.com>
2023-06-03 09:15:01 +0800

    SF-Module_7_3_1_1: Абстрактные классы и их методы, переопределение методов обязательное

---
+++ Module_7_3_1_1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Module_7_3_1_1
+{
+    abstract class FourLeggedAnimal
+    {
+        public virtual void Describe()
+        {
+            Console.WriteLine("Это неизвестное животное");
+        }
+    }
+
+    class Dog : FourLeggedAnimal
+    {
+        public override void Describe()
+        {
+            Console.WriteLine("Это животное - собака");
+        }
+    }
+
+    class Cat : FourLeggedAnimal
+    {
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            dog.Describe(); // Собака
+            cat.Describe(); // Неизвестное животное
+            FourLeggedAnimal animal = dog;
+            animal.Describe();
+
+            animal = cat; // Собака
+            animal.Describe(); // Неизвестное животное
+        }
+    }
+}
