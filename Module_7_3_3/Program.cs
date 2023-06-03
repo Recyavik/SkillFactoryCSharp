714535cf573cb560b3bfd9b4f489a782444675a7
Recyavik <vkataev71@gmail.com>
2023-06-03 09:32:34 +0800

    SF-Module_7_3_3: Абстрактные классы и их методы, переопределение методов обязательное

---
+++ Module_7_3_3/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Module_7_3_3
+{
+    abstract class ComputerPart
+    {   
+        public virtual void Description()
+        {
+            Console.WriteLine("Устройство компьютера неизвестно");
+        }
+    }
+    class Processor : ComputerPart
+    {
+        public override void Description()
+        {
+            Console.WriteLine("Устройство - процессор ");
+        }
+    }
+    class MotherBoard : ComputerPart
+    {
+        public override void Description()
+        {
+            Console.WriteLine("Устройство - материнская плата ");
+        }
+    }
+    class GraphicCard : ComputerPart
+    {
+        public override void Description()
+        {
+            Console.WriteLine("Устройство - видеокарта ");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Processor proc = new Processor();
+            MotherBoard mat = new MotherBoard();
+            GraphicCard card = new GraphicCard();
+
+            proc.Description();
+            mat.Description();
+            card.Description();
+
+            ComputerPart part = proc;
+            part.Description();
+
+        }
+    }
+}
