27b6920ffe9130b490719cc29250f7af50b35d8d
Recyavik <vkataev71@gmail.com>
2023-06-03 13:13:21 +0800

    SF-Module_7_5_1: MinValue - статический элемент класса

---
+++ Module_7_5_1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Module_7_5_1
+{
+    class SimpleClass
+    {
+        public static int MinValue = 100;
+
+        public int Value;
+
+        public SimpleClass(int value)
+        {
+            if (value < MinValue)
+            {
+                Value = MinValue;
+            }
+            else
+            {
+                Value = value;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SimpleClass.MinValue);
+
+            SimpleClass object1 = new SimpleClass(50);
+            SimpleClass object2 = new SimpleClass(170);
+
+            Console.WriteLine(object1.Value); // 100 потому что Value < MinValue
+            Console.WriteLine(object2.Value); // 170
+        }
+    }
+}
