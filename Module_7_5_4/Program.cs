5b0bac14a350a80b564904f08dbf1b02fb39fb52
Recyavik <vkataev71@gmail.com>
2023-06-03 14:50:54 +0800

    SF-Module_7_5_4: Cтатический конструктор класса вызывается автоматически при создании экземпляра класса

---
+++ Module_7_5_4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Module_7_5_4
+{
+    class Car
+    {
+        public static int MinPrice = 100_000;
+        public static int MaxPrice;
+
+        static Car()
+        {
+            Console.WriteLine("Вызван статический конструктор класса Car");
+            MaxPrice = 1_000_000;
+        }
+
+        public int Price;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            Console.WriteLine(Car.MinPrice);
+
+            Console.WriteLine(Car.MaxPrice);
+        }
+    }
+}
