86631ee214cb7752726589b83b479242a7edab3e
Recyavik <vkataev71@gmail.com>
2023-06-03 13:53:52 +0800

    SF-Module_7_5_3: Swap - статический метод класса

---
+++ Module_7_5_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Intrinsics;
+
+namespace Module_7_5_3
+{
+    class Helper
+    {
+        public int v1;
+        public int v2;
+
+        public static void Swap(ref int v1, ref int v2)
+        { 
+            int b;
+            b = v1;
+            v1 = v2;
+            v2 = b;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 3;
+            int num2 = 58;
+
+            Helper.Swap(ref num1, ref num2);           /*передача num1 и num2 в метод*/
+            
+
+            Console.WriteLine(num1); //58
+            Console.WriteLine(num2); //3
+        }
+    }
+}
