851c3b817790622cbefe0b3281765d80ce15f7e2
Recyavik <vkataev71@gmail.com>
2023-06-03 17:00:38 +0800

    SF-Module_7_5_5: Cтатические классы работают только со статистическими методами, полями, свойствами

---
+++ Module_7_5_5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Module_7_5_5
+{
+    static class Car
+    {
+        public static int MinPrice = 100_000;
+        public static int MaxPrice;
+
+        static Car()
+        {
+            Console.WriteLine("Вызван статический конструктор класса Car");
+            MaxPrice = 1_000_000;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Car.MaxPrice);
+        }
+    }
+}
