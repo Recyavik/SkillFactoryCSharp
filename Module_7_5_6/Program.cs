e30353d4981b7a231329e737765702a6ddae9b45
Recyavik <vkataev71@gmail.com>
2023-06-04 13:10:34 +0800

    SF-Module_7_5_6: Методы расширения

---
+++ Module_7_5_6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Module_7_5_6
+{
+    static class StringExtensions
+    {
+        public static char GetLastChar(this string source)
+        {
+            return source[source.Length - 1];
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello";
+            Console.WriteLine(str.GetLastChar());
+
+            Console.WriteLine("Строка".GetLastChar());
+        }
+    }
+}
