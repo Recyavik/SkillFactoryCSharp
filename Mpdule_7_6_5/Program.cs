736737ba9d8d164a32018c7960aff0a5e5566434
Recyavik <vkataev71@gmail.com>
2023-06-04 14:11:31 +0800

    SF-Module_7_6_5: Обобщения использование универсальных параметров

---
+++ Mpdule_7_6_5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mpdule_7_6_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
