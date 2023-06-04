d8a1272308565fe781f661b98fe1d10ce87f3002
Recyavik <vkataev71@gmail.com>
2023-06-04 18:11:36 +0800

    SF-Module_7_6_10: Наследование обобщений с указанием типа

--- Module_7_6_10/Program.cs
+++ Module_7_6_10/Program.cs
@@ -1,10 +1,21 @@
 ﻿namespace Module_7_6_10
 {
+    class BaseClass<T>
+    {
+        public T Field;
+    }
+    class DerivedClass<T> : BaseClass<int>
+    {
+        public T Property { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DerivedClass<string> derived = new DerivedClass<string>();
+            derived.Field = 13;     // Тип int
+            derived.Property = "22";	// Тип string
         }
     }
 }
