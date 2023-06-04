bf6ab4afe23307db020e66021cf6d6cfcdffe8f3
Recyavik <vkataev71@gmail.com>
2023-06-04 18:02:49 +0800

    SF-Module_7_6_10: Наследование обобщений

---
+++ Module_7_6_10/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module_7_6_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
