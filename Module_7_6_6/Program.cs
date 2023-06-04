db3d86ce77addde5d93cd7276efe154079d0bb1f
Recyavik <vkataev71@gmail.com>
2023-06-04 14:27:25 +0800

    SF-Module_7_6_6: Обобщённые методы

---
+++ Module_7_6_6/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Module_7_6_6
+{
+    class Obj
+    {
+        public void Display<T>(T param)
+        {
+            Console.WriteLine(param.ToString());
+        }
+        public static void Swap<T>(ref T x, ref T y)
+        {
+            T t = x;
+            x = y;
+            y = t;
+        }
+    }
+    class Proga
+    {
+        public static void Swap<T>(ref T x, ref T y)
+        {
+            T t = x;
+            x = y;
+            y = t;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Obj obj = new Obj();
+                obj.Display<int>(345);
+
+                int num1 = 4;
+                int num2 = 10;
+
+                Proga.Swap<int>(ref num1, ref num2);
+
+                Console.WriteLine("{0} {1}", num1, num2);
+        }
+    }
+}
--- Mpdule_7_6_5/Program.cs
+++ Mpdule_7_6_5/Program.cs
@@ -1,10 +1,22 @@
 ﻿namespace Mpdule_7_6_5
 {
+    class Generic<T1, T2, T3>
+    {
+        public T1 Field; // Поле типа T1
+
+        public T2 Property { get; set; } // Свойство типа T2
+
+        public void Method(T3 param) // Параметр типа T3
+        {
+
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Generic<object, int, string> generic = new Generic<object, int, string>();
         }
     }
 }
