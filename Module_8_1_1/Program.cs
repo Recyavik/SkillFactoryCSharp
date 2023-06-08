80071f4e450b6b5da1d904706f3bd4ca7fc05d6f
Recyavik <vkataev71@gmail.com>
2023-06-08 13:15:33 +0800

    SF-Module_8_1_1 Файловая система - класс DriveInfo

---
+++ Module_8_1_1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Module_8_1_1
+{
+    public class Drive
+    {
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+
+        public string Name { get; }
+        public long TotalSpace { get; }
+        public long FreeSpace { get; }
+       enum DriveType {USB, HDD, CD}
+}
+    public class Folder
+    {
+        public Folder(string name)
+        {
+            Name = name;
+        }
+
+        private string Name { get; set; }
+        List<string> Files { get; set; } = new List<string>();
+
+        void AddFile(string name)
+        {
+            if (!Files.Contains(name))
+                Files.Add(name);
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
