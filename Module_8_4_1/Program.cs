f219968e5470c0424ea90bd446f2d59fd8e3792f
Recyavik <vkataev71@gmail.com>
2023-06-11 21:56:17 +0800

    SF-Module_8_4_1 Запись и считывание файлов в Бинарном формате потока из файла BinaryFile.bin

---
+++ Module_8_4_1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Module_8_4_1
+{
+    internal class Program
+    {
+        const string SettingsFileName = @"E:\Develop\C#\SkillFactoryCSharp\SkillFactoryCSharp\Module_8_4_1\BinaryFile.bin";
+        static void Main(string[] args)
+        {
+            // Считываем
+            ReadValues();
+            WriteValues();
+        }
+       static void WriteValues()
+        {
+            // Создаем объект BinaryWriter и указываем, куда будет направлен поток данных
+            using (BinaryWriter writer = new BinaryWriter(File.Open(SettingsFileName, FileMode.Create)))
+            {
+                // записываем данные в разном формате
+                writer.Write($"Файл изменен {DateTime.Now} на компьютере с ОС {Environment.OSVersion}");
+            }
+        }
+
+        static void ReadValues()
+        {
+            string DataValue;
+            if (File.Exists(SettingsFileName))
+            {
+                // Создаем объект BinaryReader и инициализируем его возвратом метода File.Open.
+                using (BinaryReader reader = new BinaryReader(File.Open(SettingsFileName, FileMode.Open)))
+                {
+                    // Применяем специализированные методы Read для считывания соответствующего типа данных.
+                    DataValue = reader.ReadString();
+                }
+
+                Console.WriteLine("Из файла считано:");
+                Console.WriteLine("Дата: " + DataValue);
+
+            }
+        }
+    }
+}
