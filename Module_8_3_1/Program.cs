e3ace5d4bfcfdab602040700237060b2732e3726
Recyavik <vkataev71@gmail.com>
2023-06-11 18:11:24 +0800

    SF-Module_8_3_1 Считывание из файла и вывод построчно в консоль

---
+++ Module_8_3_1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Module_8_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"E:\Develop\C#\SkillFactoryCSharp\SkillFactoryCSharp\Module_8_3_1\Program.cs"; // Укажем путь
+
+            // Откроем файл и прочитаем его содержимое
+            using StreamReader sr = File.OpenText(filePath);
+            string? str = "";
+            while ((str = sr.ReadLine()) != null)
+                Console.WriteLine(str);
+        }
+    }
+}
