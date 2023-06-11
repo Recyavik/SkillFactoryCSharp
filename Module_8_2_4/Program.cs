69fe42f15d5e46117176aa564c771ff3bcddac32
Recyavik <vkataev71@gmail.com>
2023-06-11 15:28:20 +0800

    SF-Module_8_2_4 Созданиеие каталога и его перемещение в другой каталог

---
+++ Module_8_2_4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Module_8_2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new(@"C://PrimerC#");
+                dirInfo.Create();
+                string trashPath = "C://PrimerC#2";
+                dirInfo.MoveTo(trashPath);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
+           
+        }
+    }
+}
