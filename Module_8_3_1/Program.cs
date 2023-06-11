a0aa13c986c347b4b58d7e3c96ed3706a8b8952e
Recyavik <vkataev71@gmail.com>
2023-06-11 18:37:21 +0800

    SF-Module_8_3_1 Создание(генерация) файла и вывод построчно в консоль затем скопировать в другой файл

--- Module_8_3_1/Program.cs
+++ Module_8_3_1/Program.cs
@@ -4,13 +4,46 @@
     {
         static void Main(string[] args)
         {
-            string filePath = @"E:\Develop\C#\SkillFactoryCSharp\SkillFactoryCSharp\Module_8_3_1\Program.cs"; // Укажем путь
+            string tempFile = Path.GetTempFileName(); // используем генерацию имени файла.
+            var fileInfo = new FileInfo(tempFile); // Создаем объект класса FileInfo.
 
-            // Откроем файл и прочитаем его содержимое
-            using StreamReader sr = File.OpenText(filePath);
-            string? str = "";
-            while ((str = sr.ReadLine()) != null)
-                Console.WriteLine(str);
+            //Создаем файл и записываем в него.
+            using (StreamWriter sw = fileInfo.CreateText())
+            {
+                sw.WriteLine("Игорь");
+                sw.WriteLine("Андрей");
+                sw.WriteLine("Сергей");
+            }
+
+            //Открываем файл и читаем из него.
+            using (StreamReader sr = fileInfo.OpenText())
+            {
+                string? str = "";
+                while ((str = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+
+            try
+            {
+                string tempFile2 = Path.GetTempFileName();
+                var fileInfo2 = new FileInfo(tempFile2);
+
+                // Убедимся, что файл назначения точно отсутствует
+                fileInfo2.Delete();
+
+                // Копируем информацию
+                fileInfo.CopyTo(tempFile2);
+                Console.WriteLine($"{tempFile} скопирован в файл {tempFile2}.");
+                //Удаляем ранее созданный файл.
+                fileInfo.Delete();
+                Console.WriteLine($"{tempFile} удален.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e}");
+            }
         }
     }
 }
