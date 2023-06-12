4f193cccacc666e3cfb8763b74740f06cbd489f6
Recyavik <vkataev71@gmail.com>
2023-06-12 12:52:45 +0800

    SF-Module_8_5_Itog_Task1 Программа удаления файлов и папок продолжительностью жизни 30 минут

--- Module_8_5_Itog_Task1/Program.cs
+++ Module_8_5_Itog_Task1/Program.cs
@@ -1,53 +1,66 @@
-﻿using System;
-using System.IO;
-using static System.Console;
+﻿using static System.Console;
 namespace Module_8_5_Itog_Task1
 {
+    //Напишите программу, которая чистит нужную нам папку от файлов  и папок,
+    //которые не использовались более 30 минут
     internal class Program
     {
         public static void DeleteFiles(string pathFolder, int time)
         {
             WriteLine($"Удаляем файлы в папке {pathFolder}: ");
-            DirectoryInfo dr = new(pathFolder);
-            TimeSpan interval = TimeSpan.FromMinutes(time);
-            DateTime dtNow = DateTime.Now;
-            foreach (FileInfo fl in dr.GetFiles())
+            try
             {
-                DateTime dt = File.GetLastWriteTime(fl.FullName);
-                var timeLife = dtNow - dt;
-                WriteLine(interval.ToString() + " и " + timeLife.ToString());
-                if (timeLife >= interval)
+                DirectoryInfo dr = new(pathFolder);
+                TimeSpan interval = TimeSpan.FromMinutes(time);
+                DateTime dtNow = DateTime.Now;
+                foreach (FileInfo fl in dr.GetFiles())
                 {
-                    WriteLine(fl);
-                    fl.Delete();
-
+                    DateTime dt = File.GetLastWriteTime(fl.FullName);
+                    var timeLife = dtNow - dt;
+                    WriteLine(interval.ToString() + " и " + timeLife.ToString());
+                    if (timeLife >= interval)
+                    {
+                        WriteLine(fl);
+                        fl.Delete();
+                    }
+                    else
+                    {
+                        WriteLine(fl + " - файл - создан недавно, удалению не подлежит");
+                    }
                 }
-                else
-                {
-                    WriteLine(fl + " - файл - создан недавно, удалению не подлежит");
-                }
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Проблема: {ex.Message} удаление файлов невозможно");
             }
         }
         public static void DeleteFolders(string pathFolder, int time)
         {
             WriteLine($"Удаляем вложенные папки каталога {pathFolder}: ");
-            DirectoryInfo dr = new(pathFolder);
-            TimeSpan interval = TimeSpan.FromMinutes(time);
-            DateTime dtNow = DateTime.Now;
-            foreach (DirectoryInfo dir in dr.GetDirectories())
+            try
             {
-                DateTime dt = Directory.GetLastWriteTime(dir.FullName);
-                var timeLife = dtNow - dt;
-                WriteLine(interval.ToString() + " и " + timeLife.ToString());
-                if (timeLife >= interval)
+                DirectoryInfo dr = new(pathFolder);
+                TimeSpan interval = TimeSpan.FromMinutes(time);
+                DateTime dtNow = DateTime.Now;
+                foreach (DirectoryInfo dir in dr.GetDirectories())
                 {
-                    WriteLine(dir);
-                    dir.Delete(true);
+                    DateTime dt = Directory.GetLastWriteTime(dir.FullName);
+                    var timeLife = dtNow - dt;
+                    WriteLine(interval.ToString() + " и " + timeLife.ToString());
+                    if (timeLife >= interval)
+                    {
+                        WriteLine(dir);
+                        dir.Delete(true);
+                    }
+                    else
+                    {
+                        WriteLine(dir + " - каталог создан недавно, удалению не подлежит");
+                    }
                 }
-                else
-                {
-                    WriteLine(dir + " - каталог создан недавно, удалению не подлежит");
-                }
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Проблема: {ex.Message} удаление каталогов невозможно");
             }
         }
         static void Main(string[] args)
