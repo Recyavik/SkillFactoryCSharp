7f6e4b65b71d594a477fee782b5f80a433e68938
Recyavik <vkataev71@gmail.com>
2023-06-24 11:43:20 +0800

    SF-Module_9_1_4 Примеры исключения виды исключений

---
+++ Module_9_1_4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Module_9_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.WriteLine("Блок try начался");
+                // throw new FileNotFoundException();
+            }
+            catch (Exception ex) when (ex is ArgumentNullException)
+            {
+                Console.WriteLine("Аргумент пустой");
+            }
+            catch (Exception ex) when (ex is FileNotFoundException) 
+            {
+                Console.WriteLine("Файл не найден");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally сработал!");
+            }
+
+        }
+    }
+}
