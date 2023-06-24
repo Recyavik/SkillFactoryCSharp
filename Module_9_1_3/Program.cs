06d2e2ca8fd650354ff089a79928621a2d07c371
Recyavik <vkataev71@gmail.com>
2023-06-24 11:18:43 +0800

    SF-Module_9_1_2 Примеры исключения деление на 0

---
+++ Module_9_1_3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Module_9_1_3
+{
+    internal class Program
+    {
+        static int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int result = Division(7, 0);
+                Division('7', 0);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                if (ex is DivideByZeroException)
+                {
+                    Console.WriteLine("На ноль делить нельзя...");
+                }
+                else
+                {
+                    Console.WriteLine("Непредвиденная ошибка в приложении");
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally сработал!");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
