bd39af511527244574151402aafdc0e9e0f9eb3d
Recyavik <vkataev71@gmail.com>
2023-06-26 10:47:45 +0800

    SF-Module_9_1_4 Оператор throw в исключениях

--- Module_9_1_4/Program.cs
+++ Module_9_1_4/Program.cs
@@ -7,7 +7,7 @@
             try 
             {
                 Console.WriteLine("Блок try начался");
-                // throw new FileNotFoundException();
+                Method2();
             }
             catch (Exception ex) when (ex is ArgumentNullException)
             {
@@ -17,11 +17,37 @@
             {
                 Console.WriteLine("Файл не найден");
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace.ToString());
+            }
             finally
             {
                 Console.WriteLine("Блок finally сработал!");
             }
+        }
 
+        static void Method1()
+        {
+            try 
+            {
+                throw new Exception("Внутреннее исключение");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        static void Method2()
+        {
+            try
+            {
+                Method1();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
