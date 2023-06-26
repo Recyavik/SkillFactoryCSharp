d7b846a3bbdcb8b0f4a5f00017b499d8a2ed8aef
Recyavik <vkataev71@gmail.com>
2023-06-26 17:34:15 +0800

    SF-Module_9_3_7 Делегаты

---
+++ Module_9_3_7/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Module_9_3_7
+{
+    internal class Program
+    {
+        delegate void ShowMessageDelegate();
+        delegate int SumDelegate(int a, int b, int c);
+        delegate bool CheckLengthDelegate(string row);
+
+        static void Main(string[] args)
+        {
+            ShowMessageDelegate showMessageDelegate = ShowMessage;
+            showMessageDelegate.Invoke();
+
+            SumDelegate sumDelegate = Sum;
+            int result = sumDelegate.Invoke(1, 30, 120);
+            Console.WriteLine(result);
+
+            CheckLengthDelegate checkLengthDelegate = CheckLength;
+            bool status = checkLengthDelegate.Invoke("skill_factory");
+            Console.WriteLine(status);
+
+            Console.ReadLine();
+        }
+
+        static void ShowMessage()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static bool CheckLength(string _row)
+        {
+            if (_row.Length > 3) return true;
+            return false;
+        }
+    }
+}
