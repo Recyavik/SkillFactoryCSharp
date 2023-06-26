e69045b91752d90bd8105c2b9cd480d2065d3050
Recyavik <vkataev71@gmail.com>
2023-06-26 11:27:27 +0800

    SF-Module_9_3_1 Делегаты

---
+++ Module_9_3_1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Module_9_3_1
+{
+    internal class Program
+    {
+        public delegate int SumDelegate(int a, int b, int c);
+
+        static void Main(string[] args)
+        {
+            SumDelegate sumDelegate = Sum;
+            int result = sumDelegate.Invoke(1, 10, 50);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
