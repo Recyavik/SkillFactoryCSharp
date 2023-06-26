88b8fc4a33704a7673f67d89d2e86927a3bf7b33
Recyavik <vkataev71@gmail.com>
2023-06-26 18:14:35 +0800

    SF-Module_9_3_11: Сокращенный вариант анонимных делегатов - Лямбда функция

---
+++ Module_9_3_11/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Module_9_3_11
+{
+    internal class Program
+    {
+        delegate int Calculate(int a, int c);
+        static void Main(string[] args)
+        {
+            Calculate calculation = (x, y) => x + y;
+            Console.WriteLine(calculation(10, 20));
+            Console.WriteLine(calculation(40, 20));
+            Console.Read();
+        }
+    }
+}
