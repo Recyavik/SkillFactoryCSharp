7b285779dc4a1fa2aed383079a48ccdba3370be6
Recyavik <vkataev71@gmail.com>
2023-06-26 11:00:48 +0800

    SF-Module_9_2_2 Оператор throw в исключениях

---
+++ Module_9_2_2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Module_9_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new ArgumentOutOfRangeException("Сообщение об ошибке");
+            }
+
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
