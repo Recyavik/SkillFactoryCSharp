999be88eb53ed58b12de588306837fcb957628bc
Recyavik <vkataev71@gmail.com>
2023-06-26 12:16:53 +0800

    SF-Module_9_3_4 Добавление методов в делегат

---
+++ Module_9_3_4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Module_9_3_4
+{
+    internal class Program
+    {
+        delegate void CalculateDelegate(int a, int b);
+
+        static void Main(string[] args)
+        {
+            CalculateDelegate cD = CalculatePlus;
+            cD += CalculateMinus;
+            cD.Invoke(100, 30);
+        }
+        static void CalculatePlus(int a, int b) 
+        { 
+            Console.WriteLine(a + b);
+        }
+        static void CalculateMinus(int a, int b) 
+        {
+            Console.WriteLine(a - b);
+        }
+    }
+}
