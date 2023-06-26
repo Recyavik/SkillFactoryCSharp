038e7e29654f36ace7e359867d701cf5c7fada8e
Recyavik <vkataev71@gmail.com>
2023-06-26 20:29:54 +0800

    SF-Module_9_4_2: Контравариантность пример

---
+++ Module_9_4_2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Module_9_4_2
+{
+    internal class Program
+    {
+        public delegate Animal HandlerMethod();
+        delegate void DogInfo(Dog dog);
+
+        public static Animal AnimalHander()
+        {
+            return null;
+        }
+
+        public static Dog DogHander()
+        {
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            DogInfo dogInfo = GetAnimalInfo;
+            dogInfo.Invoke(new Dog());
+
+            HandlerMethod hM = AnimalHander;
+            HandlerMethod hD = DogHander;
+        }
+        public static void GetAnimalInfo(Animal p)
+        {
+            Console.WriteLine(p.GetType());
+        }
+
+    }
+    class Animal { }
+    class Dog : Animal { }
+}
