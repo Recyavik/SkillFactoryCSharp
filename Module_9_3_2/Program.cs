11b680935fdf59aee5207e15f77201aa42467ba4
Recyavik <vkataev71@gmail.com>
2023-06-26 11:30:16 +0800

    SF-Module_9_3_2 Делегаты функция разности двух чисел

---
+++ Module_9_3_2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Module_9_3_2
+{
+    internal class Program
+    {
+        delegate int CalculateDelegate(int a, int b);
+        static void Main(string[] args)
+        {
+            CalculateDelegate calcDelegate = Calculate;
+
+            int resultOne = calcDelegate.Invoke(100, 30);
+            Console.WriteLine(resultOne);
+
+            int resultTwo = calcDelegate(100, 30);
+            Console.WriteLine(resultTwo);
+
+
+            Console.Read();
+        }
+
+        static int Calculate(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
