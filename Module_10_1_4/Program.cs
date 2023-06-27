338c607550f44004e557585b8f058e3518e2e3a1
Recyavik <vkataev71@gmail.com>
2023-06-27 17:05:52 +0800

    SF-Module_10_2_1: Явная реализация интерфейса

--- Module_10_1_4/Program.cs
+++ Module_10_1_4/Program.cs
@@ -1,10 +1,37 @@
 ﻿namespace Module_10_1_4
 {
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
         }
+    }
+    public class Manager : IManager
+    {
+        public void Create()
+        {
+        }
+
+        public void Read()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Delete()
+        {
+        }
+    }
+    public interface IManager
+    {
+        void Create();
+        void Read();
+        void Update();
+        void Delete();
     }
 }
---
+++ Module_10_2_1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Module_10_2_1
+{
+    public class NewMessenger : IWhatsApp, IViber //Используем явную реализацию интерфейса
+    {
+        void IWhatsApp.SendMessage(string messeger)
+        {
+            Console.WriteLine("{0} : {1} ", "WhatsApp", messeger);
+        }
+
+        void IViber.SendMessage(string messeger)
+        {
+            Console.WriteLine("{0} : {1} ", "Viber", messeger);
+        }
+    }
+    public interface IWhatsApp
+    {
+        public void SendMessage(string messeger);
+
+    }
+    public interface IViber
+    {
+        public void SendMessage(string messeger);
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NewMessenger newMessenger = new NewMessenger();
+
+            ((IWhatsApp)newMessenger).SendMessage("Hello World!");
+            ((IViber)newMessenger).SendMessage("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
