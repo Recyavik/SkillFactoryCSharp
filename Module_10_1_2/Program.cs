5dbd93deccc3a47872f0c67c59828394bbf9b940
Recyavik <vkataev71@gmail.com>
2023-06-27 13:57:01 +0800

    SF-Module_10_1_2: Интерфейс

---
+++ Module_10_1_2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Module_10_1_2
+{
+    internal class Program
+    {
+        static ILogger? Logger { get; set; }
+        static void Main(string[] args)
+        {
+            Logger = new Logger();
+            var woker1 = new Worker1(Logger);
+            var woker2 = new Worker2(Logger);
+            var woker3 = new Worker3(Logger);
+
+            woker1.Work();
+            woker2.Work();
+            woker3.Work();
+
+        }
+    }
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+    public class Logger : ILogger
+    {
+        public void Event(string message)
+        {
+            // Возможна отправка в базу даных или на почту
+            Console.WriteLine(message);
+        }
+        public void Error(string message)
+        {
+            // Возможна отправка в базу даных или на почту
+            Console.WriteLine(message);
+        }
+    }
+    public interface IWorker
+    {
+        void Work();
+    }
+}
---
+++ Module_10_1_2/Worker1.cs
@@ -0,0 +1,45 @@
+﻿namespace Module_10_1_2
+{
+    public class Worker1 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker1(ILogger logger)
+        {
+            Logger = logger;
+        }
+        public void Work()
+        {
+            Logger.Event("Woker 1 начал свою работу");
+            Thread.Sleep(3000);
+            Logger.Event("Woker 1 завершил свою работу");
+        }
+    }
+    public class Worker2 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker2(ILogger logger)
+        {
+            Logger = logger;
+        }
+        public void Work()
+        {
+            Logger.Event("Woker 2 начал свою работу");
+            Thread.Sleep(3000);
+            Logger.Event("Woker 2 завершил свою работу");
+        }
+    }
+    public class Worker3 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker3(ILogger logger)
+        {
+            Logger = logger;
+        }
+        public void Work()
+        {
+            Logger.Event("Woker 3 начал свою работу");
+            Thread.Sleep(3000);
+            Logger.Event("Woker 3 завершил свою работу");
+        }
+    }
+}
