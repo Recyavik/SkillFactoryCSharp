96790650f33b1f9007bc21f66cb31729799254eb
Recyavik <vkataev71@gmail.com>
2023-06-27 14:51:24 +0800

    SF-Module_10_1_4: Интерфейс

---
+++ Module_10_1_4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module_10_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
