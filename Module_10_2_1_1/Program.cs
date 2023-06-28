7d03688d0de79db7b87deeb105e8e38a4d6b9b80
Recyavik <vkataev71@gmail.com>
2023-06-28 10:04:12 +0800

    SF-Module_10_2_1_1: Явная реализация интерфейса всегда лучше

--- Module_10_2_1_1/Program.cs
+++ Module_10_2_1_1/Program.cs
@@ -6,5 +6,23 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public interface ICalculator
+        {
+            // void Solve(int number);  
+            void Solve(int numberOne, int numberTwo);
+        }
+        public class BaseCalculator : ICalculator 
+        { 
+            public void ICalculator.Solve(int number) 
+            {
+
+            }
+            public void ICalculator.Solve(int numberOne, int numberTwo)
+            {
+
+            }
+            
+        }
     }
 }
