08a8e9b94ebf842f27b68f981f56e29f80bbc1b6
Recyavik <vkataev71@gmail.com>
2023-06-28 09:55:59 +0800

    SF-Module_10_2_1_1: Неявная реализация интерфейса

---
+++ Module_10_2_1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module_10_2_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
