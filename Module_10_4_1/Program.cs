875c17f742723139b71594e7a1577a6e8bfa57cd
Recyavik <vkataev71@gmail.com>
2023-06-30 09:39:36 +0800

    SF-Module_10_4_1: Ковариантность и контравариантность интерфейсов

---
+++ Module_10_4_1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Module_10_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IGaragManager<Car, Garage> garagMenadger = new GarageManagerBase();
+            IGaragManager<Bike, Garage> garagMenadger2 = new GarageManagerBase(); // Проверка контравариантности
+            IGaragManager<Bike, House> garagMenadger3 = new GarageManagerBase(); // Проверка ковариантности
+        }
+    }
+    public class Car
+    {
+
+    }
+    public class Bike: Car // Проверка контравариантности
+    { 
+    
+    }
+    public class House
+    {
+
+    }
+    public class Garage: House // Проверка ковариантности
+    { 
+    
+    }
+    public interface IGaragManager<in T, out Z>
+    {
+        Z GetGarageInfo();
+        void Add(T car);
+    }
+    public class GarageManagerBase : IGaragManager<Car, Garage>
+    {
+        public void Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Garage GetGarageInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
